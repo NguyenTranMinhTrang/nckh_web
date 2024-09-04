26e8bb9973865f02939599d0ca7fe1dbc616c65e
KhaPahm <khapham.work@gmail.com>
2024-09-04 09:53:13 +0700

    Update Route

--- src/AppNavigation.tsx
+++ src/AppNavigation.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import { Route, Routes, HashRouter } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import RequireAuth from "./pages/RequireAuth";
